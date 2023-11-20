143896dca676287a9575b605fefa07ba21813a39
JB <bradburyjesse7@gmail.com>
2023-11-20 15:36:08 -0700

    Added comments and cleaned up code. Added README content.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 
+$(function () {
 
   // This is the code that creates the time boxes on the page.
 
@@ -81,23 +71,22 @@ $(function () {
     colorTime(i);
   }
 
+  // This will display a mesage informing the user when they have saved something to localstorage.
   var saveMessageDisplayed = false;
 
   function displaySaveNotification(message) {
     if (!saveMessageDisplayed) {
-    var saveNotification = $("<section></section>");
-    saveNotification.addClass("save-message text-center");
-    saveNotification.text(message);
-    $(".container-lg").prepend(saveNotification);
-    saveMessageDisplayed = true;
+      var saveNotification = $("<section></section>");
+      saveNotification.addClass("save-message text-center");
+      saveNotification.text(message);
+      $(".container-lg").prepend(saveNotification);
+      saveMessageDisplayed = true;
     }
   }
   // This is the button logic, it stores the ID of the text box and the value 
   // that is writen into the textentry and stores it to the local storage. 
   $(".btn").on("click", function () {
-    // var saveNotification = $("<p></p>")
-    // saveNotification.text("Appointment Added to localstorage &#x2713;");
-    // $(".statusBar").append(saveNotification);
+
     displaySaveNotification("Appointment Added to localstorage")
     console.log("Button Clicked");
 
@@ -123,37 +112,6 @@ $(function () {
 
   });
 
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-
-  // // Variable for the current time
-  // var currentTime = dayjs().hour();
-
-  // // We need to set up some if statements that will check the time ID of the box to 
-  // // the currentTime varriable. Then adds the class of past present or future. 
-
-  // // We need to make the variable that will get a comparable value from the box div
-  // function colorTime() {
-  //   if (currentTime > startingTime) {
-  //     // Make color green
-  //   }
-  //   if (currentTime == startingTime) {
-  //     // Make color red
-  //   }
-  //   if (currentTime < boxTime) {
-  //     // Make color gray
-  //   }
-  // }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
   $(document).ready(function () {
     // Retrieve saved tasks from local storage
     var savedSchedule = JSON.parse(localStorage.getItem("savedSchedule")) || [];
@@ -166,7 +124,7 @@ $(function () {
   });
 
 
-  // TODO: Add code to display the current date in the header of the page.
+  // This displays the current date in the header of the page.
   var currentDate = dayjs().format("dddd, MMMM DD");
 
   $("#currentDay").text(currentDate);
